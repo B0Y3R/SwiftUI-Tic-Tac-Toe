cbab8a3eb73badece3cf67fa452455d5ec8236c2
James Boyer <jamesboyer92@user.noreply.github.com>
2021-12-28 11:40:45 -0500

    Initial Commit

---
+++ SwiftUI-TIK-TAC-TOE/SwiftUI_TIK_TAC_TOEApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TIK_TAC_TOEApp.swift
+//  SwiftUI-TIK-TAC-TOE
+//
+//  Created by James Boyer on 12/28/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TIK_TAC_TOEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
